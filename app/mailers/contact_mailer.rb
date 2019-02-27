1a6329de7c521a12abece7d90cc9cdef114b5119
Momoko_Takita <momoko421@gmail.com>
2019-02-27 09:09:20 +0000

    commit

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(main, email)
    @main = main
    @email = email
-   mail to: "x", subject: "投稿の確認メール"
+   mail to: @email , subject: "投稿の確認メール"
   end
 end
